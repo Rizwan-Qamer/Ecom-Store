68901727876d8071447a3e5b17335ac27230eda4
Rizwan Qamer <rizwanqamer321@gmail.com>
2024-12-04 15:52:58 +0500

    Create base/about/navbar.html or Add Content

--- store/views.py
+++ store/views.py
@@ -10,3 +10,10 @@ def home(request):
     }
     
     return render ( request, 'home.html', context)
+
+def about(request):
+    
+    context = {
+        
+    }
+    return render(request, 'about.html', context)

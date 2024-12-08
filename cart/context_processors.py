de7211a59052c66ab422ed7808649544edbffea0
Rizwan Qamer <rizwanqamer321@gmail.com>
2024-12-08 23:57:00 +0500

    Add Billing Method

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -1,7 +1,6 @@
-from .cart import *
-
+from .cart import Cart
 
 # Create context processor so our cart can work on all pages of the site
 def cart(request):
-    # Return the default data from out cart
-    return {'cart': Cart(request)}
+	# Return the default data from our Cart
+	return {'cart': Cart(request)}
